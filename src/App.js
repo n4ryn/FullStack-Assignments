58bf6cef35dbb3e5cfeaceda58efd58d1c2185bb
n4ryn <vinaykumar.cs19@aryacollege.in>
2022-05-14 09:55:54 +0530

    another commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-// import React, {useState} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [num, setNum] = useState(0);
 
+  function onAdd(){
+    const updatedValue = num + 1;
+    setNum(updatedValue);
+  }
+
+  function onSub(){
+    const updatedValue = num - 1;
+    setNum(updatedValue);
+  }
 
   return (
     <>
-
+      <button type='submit' onClick={onAdd}>Add</button>
+      <button type='submit' onClick={onSub}>Sub</button>
+      <p>{num}</p>
     </>
   );
 }
