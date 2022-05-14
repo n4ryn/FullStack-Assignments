264334d1c8780e5184cf23e624782376efb7eace
n4ryn <vinaykumar.cs19@aryacollege.in>
2022-05-14 09:44:21 +0530

    Add Sub

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [num, setNum] = useState(0);
+
+  function onAdd(){
+    const updatedValue = num + 1;
+    setNum(updatedValue);
+  }
+
+  function onSub(){
+    const updatedValue = num - 1;
+    setNum(updatedValue);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button type='submit' onClick={onAdd}>Add</button>
+      <button type='submit' onClick={onSub}>Sub</button>
+      <p>{num}</p>
+    </>
   );
 }
 
