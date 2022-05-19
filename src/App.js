b08d3e5939bd98898f14d0f543ef1b615be582b0
n4ryn <vinaykumar.cs19@aryacollege.in>
2022-05-19 21:47:43 +0530

    counter added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   const [num, setNum] = useState(0);
+  const [value, setValue] = useState(true);
 
-  function onAdd(){
+  function onAdd() {
     const updatedValue = num + 1;
     setNum(updatedValue);
+    if (updatedValue > 0) {
+      checkValue(false);
+    }
   }
 
-  function onSub(){
+  function onSub() {
     const updatedValue = num - 1;
     setNum(updatedValue);
+    if (updatedValue === 0) {
+      checkValue(true);
+    }
   }
 
+  const checkValue = (value) => {
+    setValue(value);
+  };
+
+  const clearScreen = () => {
+    setNum(0);
+  };
+
   return (
-    <>
-      <button type='submit' onClick={onAdd}>Add</button>
-      <button type='submit' onClick={onSub}>Sub</button>
+    <div className="main">
+      <div className="arth">
+        <button type="submit" onClick={onAdd}>
+          Add
+        </button>
+        <button type="submit" onClick={onSub} disabled={value}>
+          Sub
+        </button>
+      </div>
       <p>{num}</p>
-    </>
+      <div className="clr">
+        <button type="submit" onClick={clearScreen}>
+          Clear
+        </button>
+      </div>
+    </div>
   );
 }
 
